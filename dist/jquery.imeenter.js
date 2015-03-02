df47e53b04f66e32608a4dc369d059401be3fd82
hrdaya <info@mar-cielo.com>
2015-03-03 00:08:05 +0900

    タイミングの変更や同時押し時の対策

--- dist/jquery.imeenter.js
+++ dist/jquery.imeenter.js
@@ -25,15 +25,14 @@
             // タイマー用
             var timer;
 
-            // keyupのエンターキー発行用
-            var evt = $.Event('keyup');
-            evt.keyCode = 13;
-
             // IME入力中かどうかを判断するフラグ
             var imeFlag = false;
 
             // キーアップイベントが発火したかどうかを判断するフラグ
-            var keyUpFlag = false;
+            var keyUpFlag = true;
+
+            // 直前のキーアップコード
+            var beforeKey = 0;
 
             // 捕捉するキーイベント
             var keyEvents = [
@@ -47,17 +46,17 @@
                 'cut.' + pluginName,
                 'paste.' + pluginName
             ];
+
             // 一旦イベントのoff
             this.off();
 
             // カット・ペースト時
             _this.$elm.on(otherEvents.join(' '), function (event) {
                 var $elm = $(this);
-                // 右クリックでのカット・ペーストのイベント捕捉
-                // キーボードショートカットではこのコードと別に「keyup（13）」が発火する模様
+                // 右クリックでのカット・ペーストのイベント対策
                 setTimeout(function () {
-                    // Keyupイベントの発行
-                    $elm.trigger(evt);
+                    // enter.imeEnterイベントの発行
+                    _this._trigger($elm);
                 }, 0);
             });
 
@@ -70,28 +69,45 @@
 
                 // イベントを実行しないKeyupキーコード
                 var exceptKeys = [
+                    3, // Cancel
+                    6, // Help
                     9, // TAB
                     15, // Command
                     16, // Shift
                     17, // Ctrl
                     18, // Alt
-                    19, // Pause Break
-                    20, // Shift+Caps Lock
+                    19, // Pause
+                    20, // Caps Lock
+                    21, // KANA
+                    22, // Mac"英数" キー
+                    23, // JUNJA
+                    24, // FINAL
+                    25, // KANJI
                     27, // Esc
                     28, // 「前候補・変換」
                     29, // 「無変換」
+                    30, // ACCEPT
+                    31, // MODECHANGE
                     33, // Page Up
                     34, // Page Down
                     35, // End
                     36, // Home
                     37, 38, 39, 40, // 矢印
+                    41, // SELECT
+                    42, // PRINT
+                    43, // EXECUTE
+                    44, // Print Screen
                     45, // Ins
                     91, // 左Windowsキー
                     92, // 右Windowsキー
                     93, // 右クリックキー
+                    95, // SLEEP
                     112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, // ファンクションキー
+                    124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, // ファンクションキー
                     144, // Num Lock
                     145, // Scroll Lock
+                    224, // Command
+                    225, // AltGr
                     240, 241, // Caps Lock
                     242, // 「カタカナ・ひらがな」
                     243, 244  // 「半角・全角」
@@ -104,31 +120,39 @@
                         // 一旦IME入力中フラグのリセット
                         imeFlag = false;
 
-                        if (keyCode === 229) {
-                            // キーコードが229の場合はIME入力中とみなす（Chrome・IE用）
+                        // 設定していたタイマーのリセット
+                        clearTimeout(timer);
+
+                        // キーコードが229の場合はIME入力中とみなす（Chrome・IE用）
+                        // キーコードが0の場合はIME入力開始とみなす（FF用）
+                        if (keyCode === 229 || keyCode === 0) {
                             // IME入力中フラグのセット
                             imeFlag = true;
+                        }
 
-                            // 設定していたタイマーのリセット
-                            clearTimeout(timer);
-
-                            // Chrome対策
-                            // キー入力から300ミリ秒後にkeyupイベントの
-                            // 無い場合は入力確定とする（調整必要）
+                        // Chrome対策
+                        // キーコードが229の場合
+                        // 直前のキーアップコードが「半角・全角」でない
+                        // 直前のキーアップコードが「カタカナ・ひらがな」でない
+                        // 直前のキーアップコードが「Caps Lock」でない
+                        // alt, ctrl, option, commandキーの同時押しでない
+                        if (keyCode === 229 && beforeKey !== 240 &&
+                                beforeKey !== 241 && beforeKey !== 242 &&
+                                beforeKey !== 243 && beforeKey !== 244 &&
+                                !event.altKey && !event.metaKey && !event.ctrlKey) {
+                            // キー入力から500ミリ秒後にkeyupイベントの
+                            // 無い場合は入力確定とする
+                            // 500ミリ秒はキー入力の早い人や押しっぱなし時に
+                            // keyupイベントが発火しないことへの対策
                             timer = setTimeout(function () {
                                 if (!keyUpFlag) {
-                                    // Keyupイベントの発行
-                                    $elm.trigger(evt);
+                                    // enter.imeEnterイベントの発行
+                                    _this._trigger($elm);
                                 }
-                            }, 300);
+                            }, 500);
 
                             // キーアップフラグのリセット
                             keyUpFlag = false;
-                        } else if (keyCode === 0) {
-                            // キーコードが0の場合はIME入力開始とみなす（FF用）
-                            // エンターキーを押すまでキーイベントが一切起きない
-                            // IME入力中フラグのセット
-                            imeFlag = true;
                         }
                         break;
                     case 'keypress':
@@ -140,20 +164,21 @@
                         // キーアップフラグのセット
                         keyUpFlag = true;
 
-                        // 文字入力以外のキー入力は除外
-                        // ((event.ctrlKey || event.metaKey) && keyCode === 86)はctrl+v
-                        // ((event.ctrlKey || event.metaKey) && keyCode === 88)はctrl+x
-                        if (exceptKeys.indexOf(keyCode) === -1 &&
-                                !((event.ctrlKey || event.metaKey) && keyCode === 86) &&
-                                !((event.ctrlKey || event.metaKey) && keyCode === 88)) {
+                        // 直前のキーコード確認用
+                        beforeKey = keyCode;
 
-                            // IME入力中でない
-                            // IME入力中でキーコード13が発行された
-                            if (!imeFlag ||
-                                    (imeFlag && keyCode === evt.keyCode)) {
-                                // イベントの発行
-                                _this._trigger($elm);
-                            }
+                        // 文字入力以外のキー入力は除外
+                        // alt, ctrl, option, commandキーの同時押しの場合は除外
+                        if (exceptKeys.indexOf(keyCode) !== -1 ||
+                                event.altKey || event.metaKey || event.ctrlKey) {
+                            return;
+                        }
+
+                        // IME入力中でない
+                        // IME入力中でキーコード13が発行された
+                        if (!imeFlag || (imeFlag && keyCode === 13)) {
+                            // enter.imeEnterイベントの発行
+                            _this._trigger($elm);
                         }
                         break;
                 }
